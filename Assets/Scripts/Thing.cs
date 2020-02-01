c4d688eba31d8bcfedf0cd881974d283b772a26e
BruReale <brunodm.reale@gmail.com>
2020-02-01 17:02:53 -0300

    Added repaired bar

--- Assets/Scripts/Thing.cs
+++ Assets/Scripts/Thing.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Thing : MonoBehaviour
 {
@@ -14,16 +15,26 @@ public class Thing : MonoBehaviour
 
     float m_timePassed;
 
+    [SerializeField]Image fillBar;
+
+    float actualPercent;
+
     // Start is called before the first frame update
     void Start()
     {
         m_rb = GetComponent<Rigidbody>();
+        fillBar = transform.GetChild(0).GetChild(0).GetComponent<Image>();
+
+        actualPercent = fillBar.fillAmount * m_neededToRepair;
     }
 
     // Update is called once per frame
     void Update()
     {
         m_timePassed += Time.deltaTime;
+
+        if (fillBar.fillAmount > 0)
+            fillBar.fillAmount -= Time.deltaTime / 100f;
     }
 
 
@@ -37,16 +48,26 @@ public class Thing : MonoBehaviour
 
             Debug.Log("Punch force: " + mag.magnitude);
 
-            if (mag.magnitude > .15f)
+            if (mag.magnitude > 1.2f)
             {
+                GiveForce(other.gameObject.transform, mag.magnitude);
                 RepairConstruct();
             }
         }
     }
 
+    void GiveForce(Transform handPos, float magnitudeForce)
+    {
+        Vector3 forceImpulse = transform.position - handPos.position;
+
+        GetComponent<Rigidbody>().AddForce(forceImpulse * magnitudeForce * 10f, ForceMode.Impulse);
+    }
+
     public void RepairConstruct()
     {
         m_actualRepaired++;
+
+
 
         if (m_actualRepaired == m_neededToRepair)
             CompleteReparation();
@@ -60,5 +81,15 @@ public class Thing : MonoBehaviour
         block.SetColor("_BaseColor", Color.green);
 
         GetComponent<Renderer>().SetPropertyBlock(block);
+
+        
     }
+
+    IEnumerator FillBarProgress()
+    {
+
+        yield return new WaitForSeconds(0.1f);
+    }
+
+    
 }
